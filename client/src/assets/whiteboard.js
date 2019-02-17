656cb2f4b9162fdcf88d26a2861c23c74948950a
EQW99 <tim@timpeters.net>
2019-02-17 12:13:14 +0100

    Added drawing

--- client/src/assets/whiteboard.js
+++ client/src/assets/whiteboard.js
@@ -10,22 +10,41 @@ export default class Whiteboard {
 
         //Drawing object
         this.ctx = this.canvas.getContext('2d');
-        this.redraw();
+        this.render();
         this.ctx.scale(devicePixelRatio, devicePixelRatio);
 
+        this.ctx.lineCap = 'round';
+		this.ctx.lineJoin= 'round';
         this.mouseDown = false;
-        this.mouseX = 0;
-        this.mouseY = 0;
 
-        this.history = [];
+        this.history = []; //Last object in history is pixels; when line is vectorised, the object gets replace by the svg
 
-        //this.listen();
-        //Test
+        this.listen();
     }
 
     render() {
         this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
-		this.drawBgDots();
+        this.drawBgDots();
+
+        if (this.history.length > 0) {
+            
+            for (let i = 0; i < this.history.length; i++) { // i = line object
+                let line = this.history[i];
+                for (let j = 0; j < line.length; j++) { // j = dot object
+                
+                    let dot = line[i];
+                    this.ctx.beginPath();
+                    if(dot && dot.dragging){
+                    this.ctx.moveTo(line[i-1].x, line[i-1].y);
+                    }else{
+                    this.ctx.moveTo(line[i].x - 1, line[i].y);
+                    }
+                    this.ctx.lineTo(dot.x, dot.y);
+                    this.ctx.closePath();
+                    this.ctx.stroke();
+                }
+            }
+        }
     }
 
     drawBgDots(){
@@ -43,5 +62,48 @@ export default class Whiteboard {
 				}
 			}
 		}
-	}
+    }
+    
+    listen() { //Define event listeners
+
+        this.canvas.addEventListener('mousedown', e => {
+            this.history.push(new Object);
+            this.mouseDown = true;
+            let mouseX = e.pageX;
+            let mouseY = e.pageY;
+
+            this.addPoint(mouseX, mouseY);
+            this.render();
+        });
+
+        this.canvas.addEventListener('mouseup', () => {
+            this.mouseDown = false;
+            //this.vectorise();
+        });
+
+        this.canvas.addEventListener('mouseleave', () => {
+            this.mouseDown = false;
+            //this.vectorise();
+        });
+
+        this.canvas.addEventListener('mousemove', e => {
+            if (this.mouseDown) {
+                this.addPoint(mouseX, mouseY, true);
+            }
+            this.render();
+        });
+        
+    }
+
+    addPoint(x, y, dragging) {
+        let newDot = {
+            x,
+            y,
+            dragging,
+            strokeStyle: '#000000', //room.colour
+            lineWidth: '5', //room.thickness
+            tool: 'pen', //room.tool
+        }
+        this.history[this.history.length].push(newDot); //Adds a new dot object to the last line object in history array
+    }
 }
